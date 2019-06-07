d3facf20313d239f8b411ee5b03d9fd3602887d3
Seep G <seep.torque@gmail.com>
2019-06-07 20:38:14 +1000

    api functional - getting all tickets
    trial run successful.

--- api/src/index.js
+++ api/src/index.js
@@ -4,18 +4,43 @@ import axios from 'axios';
 import 'babel-polyfill';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
-
+dotenv.config()
 const app = express()
-
 app.use(cors({
 	exposedHeaders: ["Link"]
 }));
-
 app.use(bodyParser.urlencoded({ extended: false }));  
 
-const port = 8000
-
+const port = process.env.PORT;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 app.get('/', async (req, res) => res.send('Hello World!'))
+app.get('/tickets',async (req,res)=> {
+	try{
+		let pageNumber = req.query.page;
+		let per_page = req.query.per_page;
+	const resp = await axios.get(`https://codingchallengejune2019.zendesk.com/api/v2/tickets?page=${pageNumber}&per_page=${per_page}&sort_by=created_at&sort_order=desc`,
+	{
+		auth: {
+						username,
+						password
+				}
+	}
+	);
+	let result = resp.data;
+
+	res.json(result);
+	}
+	catch(err){
+		console.log(err);
+		if(err.response.status === 404){
+			res.status(404).send('No record found');
+		}
+	}
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
